59b08c51695760c17191b44f2ef8906b4e914ecb
Matheus Matias <matheus.matias.viana@hotmail.com>
2022-09-07 18:35:25 -0300

    hooks - useState

--- 01-hooks/src/App.js
+++ 01-hooks/src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+
 import './App.css';
 
+import logo from './logo.svg';
+
 function App() {
+  const [reverseRotation, setReverseRotation] = useState(false);
+  const [counter, setCounter] = useState(0);
+
+  const toggleRotation = () => {
+    setReverseRotation((reverse) => !reverse);
+  };
+
+  const incrementCounter = () => {
+    setCounter((counter) => counter + 1);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img
+          src={logo}
+          className={reverseRotation ? 'App-logo reverse' : 'App-logo'}
+          alt="logo"
+        />
+        <h4 onClick={incrementCounter}>Counter: {counter}</h4>
+        <button onClick={toggleRotation}>
+          {reverseRotation ? 'clockwise' : 'anticlockwise'}
+        </button>
       </header>
     </div>
   );
